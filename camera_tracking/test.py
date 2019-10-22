c20fb11403f2a4b8915979d9c49b0bad566260d7
Radhika Agrawal <agrawalr@usc.edu>
2019-10-21 20:48:29 -0700

    added some basic tests to do on the pi

---
+++ camera_tracking/test.py
@@ -0,0 +1,16 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+''' First test with taking pics '''
+for i in range(5):
+    sleep(5)
+    camera.capture('/home/pi/Desktop/image%s.jpg' % i)
+
+''' Record video for 5 seconds and save '''
+camera.start_recording('/home/pi/Desktop/video.h264')
+sleep(5)
+camera.stop_recording()
+
+''' Next steps: process the image to find the specific object ''' 
