4c8c0d117c3f7e6ae734c389d10c2acb7d128fcb
Andrew <andrewopem@gmail.com>
2019-10-29 05:21:30 +0000

    blah blah blah

--- camera_tracking/test.py
+++ camera_tracking/test.py
@@ -7,9 +7,11 @@ camera = PiCamera()
 for i in range(5):
     sleep(5)
     camera.capture('/home/pi/Desktop/image%s.jpg' % i)
+    print("took a picture")
 
 ''' Record video for 5 seconds and save '''
 camera.start_recording('/home/pi/Desktop/video.h264')
+print("started recording")
 sleep(5)
 camera.stop_recording()
 
