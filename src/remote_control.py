bda6cf9d27fe3b404b5c7e76103187a458faec9c
Radhika Agrawal <agrawalr@usc.edu>
2019-11-14 12:54:35 -0800

    added basic code for remote control but need to fix robotlib import

---
+++ src/remote_control.py
@@ -0,0 +1,39 @@
+# import robotlib
+# from .robotlib import Robot as robot
+# why wont my import work?? :(
+import time
+
+# note: do pip install getch if you get import errors on this
+import getch
+
+if __name__ == "__main__":
+	print("Use ASWD to control the robot. Press ESC to quit...")
+	while 1:
+		char = getch.getch()
+		if char == chr(27): #escape character
+			break
+		# print(char)
+
+		if char == chr(97): #a key pressed
+			print("left")
+			# robot.turnLeft()
+			# time.sleep(.300) #sleep for 300 ms
+			# robot.stop()
+
+		elif char == chr(100): #d key pressed
+			print("right")
+			# robot.turnRight()
+			# time.sleep(.300) #sleep for 300 ms
+			# robot.stop()
+
+		elif char == chr(119): #w key pressed 
+			print("forward")
+			# robot.forward()
+			# time.sleep(.300) #sleep for 300 ms
+			# robot.stop()
+
+		elif char == chr(115): #s key pressed
+			print("backward")
+			# robot.backward()
+			# time.sleep(.300) #sleep for 300 ms
+			# robot.stop()
