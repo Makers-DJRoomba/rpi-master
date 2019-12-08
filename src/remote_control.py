a8b05168b168fcc3572993981f95320f69a0b46e
Radhika Agrawal <agrawalr@usc.edu>
2019-12-07 17:52:19 -0800

    serial and remote stuff

--- src/remote_control.py
+++ src/remote_control.py
@@ -1,12 +1,22 @@
 import robotlib
 # why wont my import work?? :(
 import time
+import serial
 
 # note: do pip install getch if you get import errors on this
 import getch
 
 if __name__ == "__main__":
-	robot = robotlib.robot.Robot()
+
+	ser = serial.Serial(
+		port='/dev/ttyACM0',
+		baudrate=9600,
+		parity=serial.PARITY_NONE,
+		stopbits=serial.STOPBITS_ONE,
+		bytesize=serial.EIGHTBITS,
+		timeout=1
+	)
+	# robot = robotlib.robot.Robot()
 	print("Use ASWD to control the robot. Press ESC to quit...")
 	while 1:
 		char = getch.getch()
@@ -16,24 +26,52 @@ if __name__ == "__main__":
 
 		if char == chr(97): #a key pressed
 			print("left")
-			robot.turnLeft()
-			time.sleep(.300) #sleep for 300 ms
-			robot.stop()
+			# robot.turnLeft()
+			ser.write(bytes('1\n'), 'utf-8')
+			x = ser.readline().decode('utf-8')
+			x = int(x) - 48
+			print(x)
+			# time.sleep(.300) #sleep for 300 ms
+			# robot.stop()
+			# ser.write(bytes('0'), 'utf-8')
+			# x = ser.readline().decode('utf-8')
+			# x = int(x) - 48
+			# print(x)
 
 		elif char == chr(100): #d key pressed
 			print("right")
-			robot.turnRight()
-			time.sleep(.300) #sleep for 300 ms
-			robot.stop()
+			# robot.turnRight()
+			ser.write(bytes('2\n'), 'utf-8')
+			x = ser.readline().decode('utf-8')
+			x = int(x) - 48
+			print(x)
+			# time.sleep(.300) #sleep for 300 ms
+			# robot.stop()
 
 		elif char == chr(119): #w key pressed 
 			print("forward")
-			robot.forward()
-			time.sleep(.300) #sleep for 300 ms
-			robot.stop()
+			# robot.forward()
+			ser.write(bytes('3\n'), 'utf-8')
+			x = ser.readline().decode('utf-8')
+			x = int(x) - 48
+			print(x)
+			# time.sleep(.300) #sleep for 300 ms
+			# robot.stop()
 
 		elif char == chr(115): #s key pressed
 			print("backward")
-			robot.backward()
-			time.sleep(.300) #sleep for 300 ms
-			robot.stop()
+			# robot.backward()
+			ser.write(bytes('4\n'), 'utf-8')
+			x = ser.readline().decode('utf-8')
+			x = int(x) - 48
+			print(x)
+			# time.sleep(.300) #sleep for 300 ms
+			# robot.stop()
+		
+		elif char == chr(32):
+			print("stop")
+			# robot.stop()
+			ser.write(bytes('0'), 'utf-8')
+			x = ser.readline().decode('utf-8')
+			x = int(x) - 48
+			print(x)
