78c1803b8b8d54967a7900ab729d3a0791987d26
Andrew Opem <andrewopem@gmail.com>
2019-12-20 00:36:23 -0500

    convert indentation to spaces

--- src/num_remote_control.py
+++ src/num_remote_control.py
@@ -5,62 +5,62 @@ import serial
 import getch
 
 def main():
-	ser = serial.Serial(
-		port='/dev/ttyACM0',
-		baudrate=9600,
-		parity=serial.PARITY_NONE,
-		stopbits=serial.STOPBITS_ONE,
-		bytesize=serial.EIGHTBITS,
-		timeout=1
-	)
+  ser = serial.Serial(
+    port='/dev/ttyACM0',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+  )
 
-	print("Use ASWD to control the robot. Press F to stop. Use ESC to quit...")
-	while 1:
-		char = getch.getch()
+  print("Use ASWD to control the robot. Press F to stop. Use ESC to quit...")
+  while 1:
+    char = getch.getch()
 
-		# ESC key pressed
-		if char == chr(27): 
-			break
+    # ESC key pressed
+    if char == chr(27): 
+      break
 
-		# A key pressed
-		if char == chr(97): 
-			print("left")
-			ser.write(bytes('1\n'), 'utf-8')
-			x = ser.readline().decode('utf-8')
-			x = int(x) - 48
-			print(x)
+    # A key pressed
+    if char == chr(97): 
+      print("left")
+      ser.write(bytes('1\n'), 'utf-8')
+      x = ser.readline().decode('utf-8')
+      x = int(x) - 48
+      print(x)
 
-		# D key pressed
-		elif char == chr(100): 
-			print("right")
-			ser.write(bytes('2\n'), 'utf-8')
-			x = ser.readline().decode('utf-8')
-			x = int(x) - 48
-			print(x)
+    # D key pressed
+    elif char == chr(100): 
+      print("right")
+      ser.write(bytes('2\n'), 'utf-8')
+      x = ser.readline().decode('utf-8')
+      x = int(x) - 48
+      print(x)
 
-		# W key pressed 
-		elif char == chr(119): 
-			print("forward")
-			ser.write(bytes('3\n'), 'utf-8')
-			x = ser.readline().decode('utf-8')
-			x = int(x) - 48
-			print(x)
+    # W key pressed 
+    elif char == chr(119): 
+      print("forward")
+      ser.write(bytes('3\n'), 'utf-8')
+      x = ser.readline().decode('utf-8')
+      x = int(x) - 48
+      print(x)
 
-		# S key pressed
-		elif char == chr(115): 
-			print("backward")
-			ser.write(bytes('4\n'), 'utf-8')
-			x = ser.readline().decode('utf-8')
-			x = int(x) - 48
-			print(x)
-		
-		# F key pressed
-		elif char == chr(32): 
-			print("stop")
-			ser.write(bytes('0'), 'utf-8')
-			x = ser.readline().decode('utf-8')
-			x = int(x) - 48
-			print(x)
+    # S key pressed
+    elif char == chr(115): 
+      print("backward")
+      ser.write(bytes('4\n'), 'utf-8')
+      x = ser.readline().decode('utf-8')
+      x = int(x) - 48
+      print(x)
+    
+    # F key pressed
+    elif char == chr(32): 
+      print("stop")
+      ser.write(bytes('0'), 'utf-8')
+      x = ser.readline().decode('utf-8')
+      x = int(x) - 48
+      print(x)
 
 if __name__ == "__main__":
-	main()
+  main()
--- src/remote_control.py
+++ src/remote_control.py
@@ -5,39 +5,40 @@ import serial
 import getch
 
 def main():
-	robot = robotlib.robot.Robot()
-		print("Use ASWD to control the robot. Press F to stop. Use ESC to quit...")
-		while 1:
-			char = getch.getch()
+  robot = robotlib.robot.Robot()
+  print("Use ASWD to control the robot. Press F to stop. Use ESC to quit...")
+  
+  while 1:
+    char = getch.getch()
 
-			# ESC key pressed
-			if char == chr(27):
-				break
+    # ESC key pressed
+    if char == chr(27):
+      break
 
-			# A key pressed
-			if char == chr(97): 
-				print("left")
-				robot.turnLeft()
+    # A key pressed
+    if char == chr(97): 
+      print("left")
+      robot.turnLeft()
 
-			# D key pressed
-			elif char == chr(100): 
-				print("right")
-				robot.turnRight()
+    # D key pressed
+    elif char == chr(100): 
+      print("right")
+      robot.turnRight()
 
-			# W key pressed 
-			elif char == chr(119): 
-				print("forward")
-				robot.forward()
+    # W key pressed 
+    elif char == chr(119): 
+      print("forward")
+      robot.forward()
 
-			# S key pressed
-			elif char == chr(115):
-				print("backward")
-				robot.backward()
-			
-			# F key pressed
-			elif char == chr(32):
-				print("stop")
-				robot.stop()
+    # S key pressed
+    elif char == chr(115):
+      print("backward")
+      robot.backward()
+    
+    # F key pressed
+    elif char == chr(32):
+      print("stop")
+      robot.stop()
 
 if __name__ == "__main__":
-	main()
+  main()
