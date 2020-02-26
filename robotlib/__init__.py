5588021f321c86e4141d622aba88a920a19541af
Andrew <andrewopem@gmail.com>
2020-02-26 06:12:41 +0000

    fix ultrasonic test

--- robotlib/__init__.py
+++ robotlib/__init__.py
@@ -1,7 +1,3 @@
 # __init__.py
-import robotlib.camera
-import robotlib.i2c
-import robotlib.object_detection
-import robotlib.robot
 import robotlib.ultrasonic
 import robotlib.pid_controller
--- tests/ultrasonic_test.py
+++ tests/ultrasonic_test.py
@@ -1,16 +1,18 @@
 import robotlib
+import time
 
-TRIG_PIN = 1
-ECHO_PIN = 2
+TRIG_PIN = 3
+ECHO_PIN = 5
 
 def main():
   print("setting up ultrasonic ranger...")
-  ultrasonic = Ultrasonic(TRIG_PIN, ECHO_PIN)
+  ultrasonic = robotlib.ultrasonic.Ultrasonic(TRIG_PIN, ECHO_PIN)
 
   print("testing ultrasonic ranger...")
   while True:
     d = ultrasonic.getDistance()
     print("calculated distance = " + str(d))
+    time.sleep(1)
 
 if __name__ == "__main__":
   main()
